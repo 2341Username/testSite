2c5c45b6f1c52fb33b58c6f92ab273a497159d85
Lsnowl92 <lsnowzbs@gmail.com>
2022-07-18 15:20:45 +0300

    5th commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-$(document).ready((function(){$(".sliderblock__inner").slick({arrows:!1,dots:!0})}));
+$(document).ready((function(){$(".sliderblock__inner").slick({arrows:!1,dots:!0})})),$(document).ready((function(){$(".header__burger").click((function(e){$(".header__burger,.header__menu").toggleClass("active"),$("body").toggleClass("lock")}))}));
